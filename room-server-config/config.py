3001d9fda818ee9b0c606635d728e44cbf9fd7c7
Spotlight <spotlight@joscomputing.space>
2020-11-06 23:56:00 -0600

    Add Elasticsearch

--- room-server-config/config.py
+++ room-server-config/config.py
@@ -4,3 +4,5 @@ import os
 db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres/{os.getenv('POSTGRES_DB')}"
 secret_key = os.getenv("ROOM_SECRET_KEY")
 underground_enabled = os.getenv("ROOM_UNDERGROUND_ENABLED") == '1'
+# This option should remain as-is without configuration if the default Compose definition is being used.
+elasticsearch_url = "elastic:9200"

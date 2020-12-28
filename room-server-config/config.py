14264a9d88bebbda3803b94d5884186decad3861
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-12-27 21:27:28 -0500

    Update config.py

--- room-server-config/config.py
+++ room-server-config/config.py
@@ -6,3 +6,4 @@ secret_key = os.getenv("ROOM_SECRET_KEY")
 underground_enabled = os.getenv("ROOM_UNDERGROUND_ENABLED") == "1"
 # This option should remain as-is without configuration if the default Compose definition is being used.
 elasticsearch_url = "elastic:9200"
+video_deletion_enabled = False # We use 2 servers, so we disable this here.

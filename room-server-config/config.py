4ac605353965f598ffc0d19c28583b4d2a9377b8
Spotlight <spotlight@joscomputing.space>
2021-01-05 14:33:51 -0600

    Update to match room-server configuration
    This removes the need to have first.bin entirely.

--- room-server-config/config.py
+++ room-server-config/config.py
@@ -1,9 +1,21 @@
 # Please ensure this file has its options synchronized with the example in room-server.
 import os
 
+# Primary config for room-server
 db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres/{os.getenv('POSTGRES_DB')}"
-secret_key = os.getenv("ROOM_SECRET_KEY")
-underground_enabled = os.getenv("ROOM_UNDERGROUND_ENABLED") == "1"
 # This option should remain as-is without configuration if the default Compose definition is being used.
 elasticsearch_url = "elastic:9200"
-video_deletion_enabled = False # We use 2 servers, so we disable this here.
+
+# Used as the base domain within first.bin.
+root_domain = os.getenv('NGINX_BASE_DOMAIN')
+# TODO: make toggleable once possible
+root_https_enabled = False
+# We assume we are being proxied with the default Compose definition.
+root_separate_subdomain = True
+
+# Used to secure the web panel.
+underground_enabled = os.getenv("ROOM_UNDERGROUND_ENABLED") == "1"
+secret_key = os.getenv("ROOM_SECRET_KEY")
+
+# We use 2 servers, so we disable this here.
+video_deletion_enabled = False

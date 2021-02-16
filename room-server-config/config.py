6eb6e6133bbec3c7d817891d98659247746d3d12
Spotlight <spotlight@joscomputing.space>
2021-02-16 02:17:21 -0600

    Configure Elasticsearch credentials

--- room-server-config/config.py
+++ room-server-config/config.py
@@ -3,8 +3,11 @@ import os
 
 # Primary config for room-server
 db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres/{os.getenv('POSTGRES_DB')}"
+
 # This option should remain as-is without configuration if the default Compose definition is being used.
 elasticsearch_url = "elastic:9200"
+elasticsearch_user = os.getenv('ELASTIC_USER')
+elasticsearch_pass = os.getenv('ELASTIC_PASS')
 
 # Used as the base domain within first.bin.
 root_domain = os.getenv('NGINX_BASE_DOMAIN')
@@ -12,6 +15,7 @@ root_domain = os.getenv('NGINX_BASE_DOMAIN')
 root_https_enabled = False
 # We assume we are being proxied with the default Compose definition.
 root_separate_subdomain = True
+
 
 # Used to secure the web panel.
 underground_enabled = os.getenv("ROOM_UNDERGROUND_ENABLED") == "1"

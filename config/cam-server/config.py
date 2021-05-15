b9d96ac24cd9b4f932ef391e68ee5d65dcc03222
Spotlight <spotlight@joscomputing.space>
2021-05-14 21:02:46 -0500

    Allow for specification of a SendGrid API key

--- config/cam-server/config.py
+++ config/cam-server/config.py
@@ -2,3 +2,6 @@ import os
 
 # Primary config for cam-server
 db_url = f"postgresql://{os.getenv('CAM_DB_USER')}:{os.getenv('CAM_DB_PASSWORD')}@postgres/{os.getenv('CAM_DB_DATABASE')}"
+
+# Put your super secret SendGrid API Key here
+sendgrid_key = os.getenv("CAM_SENDGRID_KEY")

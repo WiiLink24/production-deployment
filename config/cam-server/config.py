62f5243c3e143093490462f9043ef3b29a7a1037
Spotlight <spotlight@joscomputing.space>
2021-05-21 21:23:21 -0500

    Allow cam-server Sentry integration

--- config/cam-server/config.py
+++ config/cam-server/config.py
@@ -5,3 +5,7 @@ db_url = f"postgresql://{os.getenv('CAM_DB_USER')}:{os.getenv('CAM_DB_PASSWORD')
 
 # Put your super secret SendGrid API Key here
 sendgrid_key = os.getenv("CAM_SENDGRID_KEY")
+
+# Sentry configuration for error logging.
+use_sentry = True
+sentry_dsn = os.getenv("CAM_SENTRY_DSN")

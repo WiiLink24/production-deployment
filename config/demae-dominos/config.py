381a8b8357e4d62b323ff7f296e78374a806abf9
noahpistilli <sketch@wiilink24.com>
2022-12-23 18:50:52 +0000

    Remove Demae Domino's

--- config/demae-dominos/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-db_url = f"postgresql://{os.getenv('DOMINOS_DB_USER')}:{os.getenv('DOMINOS_DB_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('DOMINOS_DB_DATABASE')}"
-
-# Google Maps API key for Geocoder
-google_api_key = os.getenv("DOMINOS_GOOGLE_MAPS_API_KEY")

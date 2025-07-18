d688cde1df7fae52e62387d2e2bf5845b44e53ed
Noah <75850871+noahpistilli@users.noreply.github.com>
2025-07-17 22:18:32 -0400

    Remove send grid from Digicam

--- config/cam-server/config.py
+++ config/cam-server/config.py
@@ -3,8 +3,8 @@ import os
 # Primary config for cam-server
 db_url = f"postgresql://{os.getenv('CAM_DB_USER')}:{os.getenv('CAM_DB_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('CAM_DB_DATABASE')}"
 
-# Put your super secret SendGrid API Key here
-sendgrid_key = os.getenv("CAM_SENDGRID_KEY")
+# Put your super secret SMTP Key here
+smtp_key = os.getenv("CAM_SMTP_KEY")
 
 # Sentry configuration for error logging.
 use_sentry = True
